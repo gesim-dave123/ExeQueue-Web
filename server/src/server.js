b082a260d98b32c887ed453d5b76bb1d2a2877ba
Jacinth <jacinthcedricbarral@gmail.com>
2025-09-12 10:24:47 +0800

    feat[Barral]: Configured cors to communicate with the client for tokens

--- server/src/server.js
+++ server/src/server.js
@@ -6,31 +6,29 @@ import express from 'express';
 import helmet from 'helmet';
 import morgan from 'morgan';
 import AuthRoute from '../src/routes/auth.route.js';
-import { PrismaClient } from './generated/prisma/index.js';
 
 
 dotenv.config();
-
-const prisma = new PrismaClient();
 const app = express();
 const PORT = process.env.PORT||5000;
 
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+  origin: process.env.NODE_ENV === "production" ? process.env.CORS_ORIGIN : true,  // React dev server
+  credentials: true
+}));
+
 app.use(cookie())
 app.use(morgan('dev'))
 app.use(helmet())
 
-
-
-
+// Server Routes
 app.use('/api/auth', AuthRoute)
-
-
-
-
-
 
 app.listen(PORT, () => {
     console.log('Server is running on port ', PORT);
 })
+// app.listen(PORT, '0.0.0.0', () => {
+//   console.log(`Server running on http://0.0.0.0:${PORT}`);
+// //   console.log(`Accessible from network: http://YOUR-IP:${PORT}`);
+// });
