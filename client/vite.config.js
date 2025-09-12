f16c5a2bb79bb62fb5ec20e48ff2463d3cf0ae49
Jacinth <jacinthcedricbarral@gmail.com>
2025-09-12 10:30:06 +0800

    feat[Barral]: Reconfigured to listen to any ip for local testing

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,15 @@
 import tailwindcss from '@tailwindcss/vite'
 
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+
+  //Uncomment this server part, if you want to test locally on multiple devices
+  // server: {
+  //   host: '0.0.0.0', // Listen on all network interfaces
+  //   port: 5173, // Optional: specify a port
+  // }
 })
