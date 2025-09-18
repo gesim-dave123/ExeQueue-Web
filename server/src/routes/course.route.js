2c2078c7141a0767dede45a8b6b272678be3ff1b
Jacinth <jacinthcedricbarral@gmail.com>
2025-09-19 02:04:59 +0800

    feat[Barral]: Added RequestType and course routes, and added functionalities. Fetching data from the db

---
+++ server/src/routes/course.route.js
@@ -0,0 +1,9 @@
+import express from 'express';
+import { getCourseData } from '../controllers/course.controller.js';
+
+const router = express.Router()
+
+router.get('/courses', getCourseData)
+
+
+export default router;
