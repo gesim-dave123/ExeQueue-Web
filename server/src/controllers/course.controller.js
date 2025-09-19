f52ff0062232f467b39e2d96bc0d9c4bf970a7f2
John Dave Villasan <dave.uc.main.it@gmail.com>
2025-09-20 01:07:22 +0800

    feat(queue): add endpoint to check queue status by schoolId

--- server/src/controllers/course.controller.js
+++ server/src/controllers/course.controller.js
@@ -1,34 +1,33 @@
-import prisma from "../../prisma/prisma.js";
-export const getCourseData = async(req,res) =>{
+import prisma from '../../prisma/prisma.js';
+export const getCourseData = async (req, res) => {
   try {
     const courseData = await prisma.course.findMany({
-      orderBy:{
-        courseId: 'asc'
+      orderBy: {
+        courseId: 'asc',
       },
-      select:{
+      select: {
         courseId: true,
         courseCode: true,
-        courseName: true
-      }
+        courseName: true,
+      },
     });
 
-    if(!courseData) return res.status(403).json({
-      success: false,
-      message: "Error in fetching course data"
-    })
+    if (!courseData)
+      return res.status(403).json({
+        success: false,
+        message: 'Error in fetching course data',
+      });
 
     return res.status(200).json({
-      success:true,
-      message: "Course data fetched successfully!",
-      courseData: courseData
-    })
-
+      success: true,
+      message: 'Course data fetched successfully!',
+      courseData: courseData,
+    });
   } catch (error) {
-    console.error("Error in Course Route: ", error)
+    console.error('Error in Course Route: ', error);
     return res.status(500).json({
       success: false,
-      message: "Internal Server Error!"
-    })
+      message: 'Internal Server Error!',
+    });
   }
-
-}
+};
