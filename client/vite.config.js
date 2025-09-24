cc7d5a2489fba5807aa99a80a2d438dca36b8bad
Jacinth <jacinthcedricbarral@gmail.com>
2025-09-25 00:46:26 +0800

    chore[Barral]: Configured the vite.cinfig to be able to host on any source

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,15 +1,17 @@
 import tailwindcss from '@tailwindcss/vite'
-
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-
-  //Uncomment this server part, if you want to test locally on multiple devices
-  // server: {
-  //   host: '0.0.0.0', // Listen on all network interfaces
-  //   port: 5173, // Optional: specify a port
-  // }
+  plugins: [react(), tailwindcss()],
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    strictPort: true,
+    allowedHosts: [
+      'bring-mentor-beer-liability.trycloudflare.com',
+      '.trycloudflare.com', // allow all subdomains
+      'localhost'
+    ],
+  },
 })
