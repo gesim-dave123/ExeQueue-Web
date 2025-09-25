cd825edddae0d12fc309f31ea822ea3f21b16109
Jacinth <jacinthcedricbarral@gmail.com>
2025-09-25 17:59:52 +0800

    feat[Barral]: Added a access check for client

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { validateAccess } from "../src/utils/validate.js";
+import App from './App.jsx';
+import './index.css';
+
+validateAccess();
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
