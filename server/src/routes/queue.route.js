473a98f153028864f3560e84448caa8c025dddc3
Jacinth <jacinthcedricbarral@gmail.com>
2025-10-11 16:00:47 +0800

    feat[Barral]: Added a seperate route and controller for staff queue management

---
+++ server/src/routes/queue.route.js
@@ -0,0 +1,59 @@
+import { Role } from "@prisma/client";
+import express from "express";
+import {
+  createQueueSession,
+  determineNextQueue,
+  getQueueList,
+  markQueueStatus,
+  restoreSkippedQueue,
+  viewQueues,
+} from "../controllers/queue.controller.js";
+import {
+  authenticateToken,
+  authorizeRoles,
+} from "../middlewares/auth.middleware.js";
+
+const router = express.Router();
+
+router.get(
+  "/queue/view",
+  authenticateToken,
+  authorizeRoles(Role.PERSONNEL, Role.WORKING_SCHOLAR),
+  viewQueues
+);
+
+router.get(
+  "/queue/next-queue",
+  authenticateToken,
+  authorizeRoles(Role.PERSONNEL, Role.WORKING_SCHOLAR),
+  determineNextQueue
+);
+router.post(
+  "/queue/add-session",
+  authenticateToken,
+  authorizeRoles(Role.PERSONNEL),
+  createQueueSession
+);
+router.get("/queue/queues", getQueueList);
+// router.put(
+//   "/window/assign",
+//   authenticateToken,
+//   authorizeRoles(Role.PERSONNEL, Role.WORKING_SCHOLAR),
+//   assignServiceWindow
+// );
+
+router.put(
+  "/queue/status",
+  authenticateToken,
+  authorizeRoles(Role.PERSONNEL, Role.WORKING_SCHOLAR),
+  markQueueStatus
+);
+
+router.put(
+  "/queue/restore-skipped",
+  authenticateToken,
+  authorizeRoles(Role.PERSONNEL, Role.WORKING_SCHOLAR),
+  restoreSkippedQueue
+);
+
+export default router;
