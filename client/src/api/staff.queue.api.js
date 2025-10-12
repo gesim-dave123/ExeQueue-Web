c59e53de83cb6594ccf9daf7315edeaafd8833d3
Jacinth <jacinthcedricbarral@gmail.com>
2025-10-12 14:58:52 +0800

    feat[Barral]: Added a GET api to get the queue list by status from the server

---
+++ client/src/api/staff.queue.api.js
@@ -0,0 +1,56 @@
+import axios from "axios";
+import backendConnection from "./backendConnection.js";
+
+export const studentsQueueDetails = async (queueDetails) => {
+  try {
+    if (!queueDetails) throw new Error("Queue Details is Empty!");
+
+    const response = await axios.post(
+      `${backendConnection()}/api/queue/generate`,
+      queueDetails,
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+        withCredentials: true,
+      }
+    );
+
+    if (response.data.success && response.status === 201) {
+      return {
+        success: true,
+        message: "Queue Generated",
+        queueDetails: response.data.queueDetails,
+      };
+    }
+  } catch (error) {
+    console.error("Error in Generating Queue: ", error);
+    return {
+      success: false,
+      message: "Internal Server Error",
+      queueDetails: null,
+    };
+  }
+};
+export const getQueueListByStatus = async (status) => {
+  try {
+    const response = await axios.get(
+      `${backendConnection()}/api/staff/queue/list?status=${status}`,
+      {},
+      {
+        withCredentials: true,
+      }
+    );
+    // Correct condition - check if status is 200 AND success is true
+    if (response.status === 200 && response.data.success) {
+      console.log("Queue List:", response.data.queueList);
+      return response.data.queueList;
+    } else {
+      console.warn("Unexpected response format:", response.data);
+      return [];
+    }
+  } catch (error) {
+    console.error("Error in getQueueListByStatus:", error);
+    return [];
+  }
+};
