515e5ab8bc360c31b6461747330439ab48c73da8
David-gsm123 <christiandavegesim1304@gmail.com>
2025-10-13 00:28:22 +0800

    Add forgot password with OTP verification

--- server/src/routes/auth.route.js
+++ server/src/routes/auth.route.js
@@ -1,6 +1,6 @@
 import { Role } from '@prisma/client';
 import express from 'express';
-import { createUser, loginUser, logoutUser } from '../controllers/auth.controller.js';
+import { createUser, loginUser, logoutUser,requestPasswordReset,verifyEmail,resetPassword } from '../controllers/auth.controller.js';
 import { authenticateToken, authorizeRoles } from '../middlewares/auth.middleware.js';
 // import { authorizeRoles } from '../middlewares/role.middleware.js';
 const router = express.Router();
@@ -8,5 +8,8 @@ const router = express.Router();
 router.post('/staff/login', loginUser)
 router.post('/create-account', authenticateToken, authorizeRoles(Role.PERSONNEL),createUser)
 router.post('/logout', logoutUser)
+router.post('/getOTP',requestPasswordReset)
+router.post('/verify-email',verifyEmail)
+router.patch('/reset-password',resetPassword)
 
 export default router;
