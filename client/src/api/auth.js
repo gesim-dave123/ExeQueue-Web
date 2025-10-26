1c7e339ed0fe3b39ad2297743febd6560771694b
David-gsm123 <christiandavegesim1304@gmail.com>
2025-10-26 21:26:43 +0800

    Added sendOTPtoEmail,verifyOTP and reset-Password API

--- client/src/api/auth.js
+++ client/src/api/auth.js
@@ -33,6 +33,93 @@ export const login = async (formData) => {
   }
 };
 
+export const sendOTPtoEmail = async (email) => {
+  try{
+     const response = await axios.post(
+      `${backendConnection()}/api/auth/getOTP`,
+      {email},
+      { 
+        headers: {"Content-Type" : "application/json"},
+        withCredentials: true }
+    );
+
+    if(response.status === 200){
+      showToast(response.data.message, "success");
+      return true;
+    }
+  }catch (error){
+    if (error.response) {
+      showToast(error.response.data.message || "Email not found", "error");
+    } else if (error.request) {
+      showToast("No response from server", "error");
+    } else {
+      showToast("An unexpected error occurred", "error");
+    }
+    return false;
+  }
+};
+
+export const verifyOTP = async (otp,email) => {
+  try{
+    const response = await axios.post(
+      `${backendConnection()}/api/auth/verify-email`,
+      {     
+        receivedOTP: otp,
+        email: email,          
+      },
+      { 
+        headers: {"Content-Type" : "application/json"},
+        withCredentials: true 
+      }
+    );
+
+    if(response.status === 200){
+      showToast(response.data.message, "success");
+      return true;
+    };
+
+  }catch(error){
+    if (error.response) {
+      showToast(error.response.data.message || "OTP found", "error");
+    } else if (error.request) {
+      showToast("No response from server", "error");
+    } else {
+      showToast("An unexpected error occurred", "error");
+    }
+    return false;
+  }
+};
+
+export const resetPassword = async (newPass, confirmPass, email) => {
+  try{
+    const response = await axios.post(
+      `${backendConnection()}/api/auth/reset-password`,
+      {     
+        newPassword: newPass,
+        confirmPassword: confirmPass,
+        email: email,          
+      },
+      { 
+        headers: {"Content-Type" : "application/json"},
+        withCredentials: true 
+      }
+    );
+
+    if(response.status === 200){
+      showToast(response.data.message, "success");
+      return true;
+    }
+  }catch (error){
+    if (error.response) {
+      showToast(error.response.data.message || "Passwords doesn't match", "error");
+    } else if (error.request) {
+      showToast("No response from server", "error");
+    } else {
+      showToast("An unexpected error occurred", "error");
+    }
+    return false;
+  }
+};
 export const logout = async () => {
   try {
     const response = await axios.post(
