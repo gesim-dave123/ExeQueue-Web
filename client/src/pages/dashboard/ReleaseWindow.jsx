936e2c56cb1d55ed0e38c97a8e5726066c1f4f19
jnnzz <larocojanlorenz@gmail.com>
2025-10-30 15:43:50 +0800

    fix[laroco]: fixed button in release window

--- client/src/pages/dashboard/ReleaseWindow.jsx
+++ client/src/pages/dashboard/ReleaseWindow.jsx
@@ -38,7 +38,7 @@ export default function ReleaseWindow() {
           {/* Button */}
           <button
             onClick={() => handleRelease(1)}
-            className="bg-[#1A73E8] w-full md:w-auto min-w-[150px] text-sm sm:text-base lg:text-lg text-white font-medium px-4 sm:px-6 lg:px-4 py-2 sm:py-3 lg:py-3 rounded-lg sm:rounded-xl lg:rounded-2xl transition duration-200 hover:bg-[#1669c1]"
+            className="bg-[#1A73E8] w-full md:w-auto min-w-[150px] text-sm sm:text-base lg:text-lg text-white font-medium px-4 sm:px-6 lg:px-4 py-2 sm:py-3 lg:py-3 rounded-lg sm:rounded-xl lg:rounded-2xl transition cursor-pointer duration-200 hover:bg-[#1669c1]"
           >
             Release Window 1
           </button>
@@ -59,7 +59,7 @@ export default function ReleaseWindow() {
           {/* Button */}
           <button
             onClick={() => handleRelease(2)}
-            className="bg-[#1A73E8] w-full md:w-auto min-w-[150px] text-sm sm:text-base lg:text-lg text-white font-medium px-4 sm:px-6 lg:px-4 py-2 sm:py-3 lg:py-3  rounded-lg sm:rounded-xl lg:rounded-2xl transition duration-200 hover:bg-[#1669c1]"
+            className="bg-[#1A73E8] w-full md:w-auto min-w-[150px] text-sm sm:text-base lg:text-lg text-white font-medium px-4 sm:px-6 lg:px-4 py-2 sm:py-3 lg:py-3  rounded-lg sm:rounded-xl lg:rounded-2xl transition duration-200 hover:bg-[#1669c1] cursor-pointer"
           >
             Release Window 2
           </button>
