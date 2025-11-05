5e8a1e655b81942ce17ad341491819389c695179
John Dave Villasan <dave.uc.main.it@gmail.com>
2025-11-05 21:26:13 +0800

    chore[Dave]: fixed lacking ) in queue.route.js and installed new package module in client

--- server/src/routes/queue.route.js
+++ server/src/routes/queue.route.js
@@ -1,5 +1,5 @@
-import { Role } from "@prisma/client";
-import express from "express";
+import { Role } from '@prisma/client';
+import express from 'express';
 import {
   callNextQueue,
   createQueueSession,
@@ -14,101 +14,102 @@ import {
   setRequestStatus,
   viewQueues,
   manuallyFinalizeStalledRequests,
-  manuallyCancelSkippedRequests
-} from "../controllers/queue.controller.js";
+  manuallyCancelSkippedRequests,
+} from '../controllers/queue.controller.js';
 import {
   authenticateToken,
   authorizeRoles,
-} from "../middlewares/auth.middleware.js";
+} from '../middlewares/auth.middleware.js';
 
 const router = express.Router();
 
 router.get(
-  "/view",
+  '/view',
   authenticateToken,
   authorizeRoles(Role.PERSONNEL, Role.WORKING_SCHOLAR),
   viewQueues
 );
 
 router.get(
-  "/next-queue",
+  '/next-queue',
   authenticateToken,
   authorizeRoles(Role.PERSONNEL, Role.WORKING_SCHOLAR),
   determineNextQueue
 );
 
 router.post(
-  "/add-session",
+  '/add-session',
   authenticateToken,
   authorizeRoles(Role.PERSONNEL),
   createQueueSession
 );
 
-router.get("/queues", getQueueList);
+router.get('/queues', getQueueList);
 
 // Add the new route for getting queues by status
 router.get(
-  "/list",
+  '/list',
   authenticateToken,
   authorizeRoles(Role.PERSONNEL, Role.WORKING_SCHOLAR),
   getQueueListByQuery
 );
 
 router.put(
-  "/call/:windowId",
+  '/call/:windowId',
   authenticateToken,
   authorizeRoles(Role.PERSONNEL, Role.WORKING_SCHOLAR),
   callNextQueue
 );
 
 router.put(
-  "/set/status/:queueId/:requestId/:requestStatus/:windowId",
+  '/set/status/:queueId/:requestId/:requestStatus/:windowId',
   authenticateToken,
   authorizeRoles(Role.PERSONNEL, Role.WORKING_SCHOLAR),
   setRequestStatus
 );
 router.put(
-  "/set/status/deferred/:queueId/:requestId/:windowId/:requestStatus",
+  '/set/status/deferred/:queueId/:requestId/:windowId/:requestStatus',
   authenticateToken,
   authorizeRoles(Role.PERSONNEL, Role.WORKING_SCHOLAR),
   setDeferredRequestStatus
 );
 
 router.put(
-  "/:queueId/:windowId/mark-status",
+  '/:queueId/:windowId/mark-status',
   authenticateToken,
   authorizeRoles(Role.PERSONNEL, Role.WORKING_SCHOLAR),
   markQueueStatus
 );
 
 router.put(
-  "/restore-skipped",
+  '/restore-skipped',
   authenticateToken,
   authorizeRoles(Role.PERSONNEL, Role.WORKING_SCHOLAR),
   restoreSkippedQueue
 );
 
 router.get(
-  "/current/window/:windowId",
+  '/current/window/:windowId',
   authenticateToken,
   authorizeRoles(Role.PERSONNEL, Role.WORKING_SCHOLAR),
   currentServedQueue
 );
 
 router.get(
-  "/one/:queueId",
+  '/one/:queueId',
   authenticateToken,
   authorizeRoles(Role.PERSONNEL, Role.WORKING_SCHOLAR),
   getQueue
+);
 router.post(
-  "/test/finalize-stalled",
+  '/test/finalize-stalled',
   authenticateToken,
   authorizeRoles(Role.PERSONNEL),
   manuallyFinalizeStalledRequests
 );
 
 router.post(
-  "/test/cancel-skipped",
+  '/test/cancel-skipped',
   authenticateToken,
   authorizeRoles(Role.PERSONNEL),
   manuallyCancelSkippedRequests
